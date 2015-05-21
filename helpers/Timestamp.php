483c16e498e6bbb646b75daadf8ab8320b6639af
Pascal Müller <pasci84@gmail.com>
2015-05-21 13:58:14 +0200

    has time function added

--- helpers/Timestamp.php
+++ helpers/Timestamp.php
@@ -1,6 +1,7 @@
 <?php
 namespace asinfotrack\yii2\toolbox\helpers;
 
+use yii\base\InvalidParamException;
 /**
  * Helper class for working with UNIX-timestamps
  * 
@@ -36,6 +37,31 @@ class Timestamp
 		$year = date('Y', $stamp);
 		
 		return mktime(0, 0, 0, $month, $day, $year);
+	}
+	
+	/**
+	 * Checks whether or not a timestamp has a time part. The function returns true, if
+	 * the time-part of a stamp differs from the values defined in params 2 to 4.
+	 * 
+	 * Default setting for no time part is 00:00:00.
+	 * 
+	 * @param integer $stamp the timestamp to check
+	 * @param integer $noTimeHour the hour considered as no time part
+	 * @param number $noTimeMinute the minute considered as no time part
+	 * @param number $noTimeSecond the second considered as no time part
+	 * @return boolean true if a time is set differing from the one defined in params 2 to 4
+	 * @throws InvalidParamException if values are illegal
+	 */
+	public static function hasTime($stamp, $noTimeHour=0, $noTimeMinute=0, $noTimeSecond=0)
+	{
+		//assert no time values are ok
+		if ($noTimeHour < 0 || $noTimeHour > 23 || $noTimeMinute < 0 || $noTimeMinute > 59 || $noTimeSecond < 0 || $noTimeSecond > 59) {
+			throw new InvalidParamException('Wrong values for no time hour, minute or second received');
+		}
+		
+		//get date parts and compare
+		$d = getdate($stamp);
+		return $d['hours'] != $noTimeHour || $d['minutes'] != $noTimeMinute || $d['seconds'] != $noTimeSecond;
 	}
 	
 }
