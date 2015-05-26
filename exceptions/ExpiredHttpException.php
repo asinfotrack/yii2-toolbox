ea9fcebbe554b211a5d5df5ae9dcb4ccfcb163d6
Pascal Müller <pasci84@gmail.com>
2015-05-26 16:40:16 +0200

    Added expired-http-exception

---
+++ exceptions/ExpiredHttpException.php
@@ -0,0 +1,27 @@
+<?php
+namespace asinfotrack\yii2\toolbox\exceptions;
+
+/**
+ * A HTTP-exception representing the status-code 410 (gone) which is used to mark
+ * a link as no longer valid.
+ * 
+ * @author Pascal Mueller, AS infotrack AG
+ * @link http://www.asinfotrack.ch
+ * @license MIT
+ */
+class ExpiredHttpException extends \yii\web\HttpException
+{
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param string $message error message
+	 * @param integer $code error code
+	 * @param \Exception $previous The previous exception used for the exception chaining.
+	 */
+	public function __construct($message = null, $code = 0, \Exception $previous = null)
+	{
+		parent::__construct(410, $message, $code, $previous);
+	}
+	
+}
