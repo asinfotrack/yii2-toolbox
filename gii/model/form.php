fea8a71277cfafaed589127f7deeafb31d835739
Thomas Lutzenberger <thomas.lutzenberger@asinfotrack.ch>
2017-07-21 13:21:01 +0200

    Use semantically meaningful markup

--- gii/model/form.php
+++ gii/model/form.php
@@ -6,14 +6,13 @@ use yii\helpers\Html;
 /* @var $form yii\widgets\ActiveForm */
 /* @var $generator yii\gii\generators\form\Generator */
 
-echo Html::beginTag('div', ['class'=>'page-header']);
-echo Html::tag('h2', 'Custom', ['class'=>'h3']);
-echo Html::endTag('div');
+echo Html::beginTag('fieldset');
+	echo Html::tag('legend', 'Custom');
+	echo $form->field($generator, 'iconName')->textInput(['placeholder'=>'e.g. "calendar"']);
+echo Html::endTag('fieldset');
 
-echo $form->field($generator, 'iconName')->textInput(['placeholder'=>'e.g. "calendar"']);
 
-echo Html::beginTag('div', ['class'=>'page-header']);
-echo Html::tag('h2', 'Standard', ['class'=>'h3']);
-echo Html::endTag('div');
-
-require(Yii::getAlias('@vendor/yiisoft/yii2-gii/generators/model/form.php'));
+echo Html::beginTag('fieldset');
+	echo Html::tag('legend', 'Standard');
+	require(Yii::getAlias('@vendor/yiisoft/yii2-gii/generators/model/form.php'));
+echo Html::endTag('fieldset');
