6199ac87f83d0f73e35a95cda87fb31f8f720af8
Pascal Müller <pasci84@gmail.com>
2017-11-06 10:52:41 +0100

    Bugfix in archive behavior where already set archived value would be overwritten during insert

--- behaviors/ArchiveBehavior.php
+++ behaviors/ArchiveBehavior.php
@@ -10,36 +10,36 @@ use yii\db\ActiveRecord;
  * archived or unarchived.
  * To use the behavior simply attach it to your model and specify the column holding
  * the archive-value.
- * 
+ *
  * @author Pascal Mueller, AS infotrack AG
  * @link http://www.asinfotrack.ch
  * @license MIT
  */
 class ArchiveBehavior extends \yii\base\Behavior
 {
-	
+
 	/**
 	 * @var string name of the column holding the boolean flag to determine
 	 * if a record is archived or not.
 	 */
 	public $archiveAttribute = 'is_archived';
-	
+
 	/**
 	 * @var mixed|null holds the default value which will be set upon creation
 	 * of a record. If set to null, no value is set.
 	 */
 	public $defaultArchiveValue = 0;
-	
+
 	/**
 	 * Holds the value marking a record as archived (defaults to 1)
 	 */
 	public $archivedValue = 1;
-	
+
 	/**
 	 * Holds the value marking a record as NOT archived (defaults to 0)
 	 */
 	public $unarchivedValue = 0;
-	
+
 	/**
 	 * @inheritdoc
 	 */
@@ -49,7 +49,7 @@ class ArchiveBehavior extends \yii\base\Behavior
 			ActiveRecord::EVENT_BEFORE_INSERT=>'onBeforeInsert',
 		];
 	}
-	
+
 	/**
 	 * @inheritdoc
 	 * @param \yii\db\ActiveRecord $owner
@@ -63,24 +63,24 @@ class ArchiveBehavior extends \yii\base\Behavior
 		if ($owner->tableSchema->getColumn($this->archiveAttribute) === null) {
 			throw new InvalidConfigException(sprintf('The table %s does not contain a column named %s', $owner->tableName(), $this->archiveAttribute));
 		}
-	
+
 		parent::attach($owner);
 	}
-	
+
 	/**
 	 * This method is called before a record is inserted. It sets the column to
 	 * the default value if there is one specified
 	 */
 	public function onBeforeInsert()
 	{
-		if ($this->defaultArchiveValue === null) return;
+		if ($this->defaultArchiveValue === null || !empty($this->owner->{$this->archiveAttribute})) return;
 		$this->owner->{$this->archiveAttribute} = $this->defaultArchiveValue;
 	}
-	
+
 	/**
 	 * Archives a record. If the second param is set to true (default),
 	 * the model will be saved immediately.
-	 * 
+	 *
 	 * @param boolean $saveImmediately if set to true, the model will be saved
 	 * immediately (defaults to true)
 	 * @return boolean true if archiving was successful (if $saveImmediately was
@@ -91,11 +91,11 @@ class ArchiveBehavior extends \yii\base\Behavior
 		$this->owner->{$this->archiveAttribute} = $this->archivedValue;
 		return $saveImmediately ? $this->owner->save() : true;
 	}
-	
+
 	/**
 	 * Unarchives a record. If the second param is set to true (default),
 	 * the model will be saved immediately.
-	 * 
+	 *
 	 * @param boolean $saveImmediately if set to true, the model will be saved
 	 * immediately (defaults to true)
 	 * @return boolean true if archiving was successful (if $saveImmediately was
@@ -106,15 +106,15 @@ class ArchiveBehavior extends \yii\base\Behavior
 		$this->owner->{$this->archiveAttribute} = $this->unarchivedValue;
 		return $saveImmediately ? $this->owner->save() : true;
 	}
-	
+
 	/**
 	 * Simple getter to decide if a record is archived or not
-	 * 
+	 *
 	 * @return boolean true if archived, otherwise false
 	 */
 	public function getIsArchived()
 	{
 		return $this->owner->{$this->archiveAttribute} == $this->archivedValue;
 	}
-	
+
 }
