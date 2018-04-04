79cc328e4ce8996c5e99bacf470d31d68e287e26
pasci84 <pasci84@gmail.com>
2018-04-04 20:57:34 +0200

    changing return value to metres

--- helpers/Coordinates.php
+++ helpers/Coordinates.php
@@ -13,13 +13,13 @@ class Coordinates
 
 	public static function distanceHaversine($lat1, $lon1, $lat2, $lon2) 
 	{
-		$r = 6371;
+		$earthRadius = 63711000;
 		$dLat = deg2rad($lat2 - $lat1);
 		$dLong = deg2rad($lon2 - $lon1);
 
 		$a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLong / 2) * sin($dLong / 2);
 		$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-		$d = R * $c;
+		$d = $earthRadius * $c;
 
 		return $d;
 	}
